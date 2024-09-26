46263862cce3a47033869be4fd95633adaaa25db
Luis Miguel Mackiewicz <159212359+Luis-Mackiewicz@users.noreply.github.com>
2024-09-26 17:33:49 -0300

    "Adiciondo API ao repositório"

---
+++ php/main.php
@@ -0,0 +1,8 @@
+<main class="main">
+    <div class="width_main">
+        <div class="main_info">
+            <p class="introducion">Você se aventura na floresta enigmática em busca de um artefato lendário perdido há séculos. À medida que avança, encontra uma bifurcação: uma trilha à esquerda leva a uma pequena casa onde um sábio eremita pode oferecer um valioso mapa; a trilha à direita conduz a uma caverna obscura onde, ao acender uma tocha, você descobre um baú escondido. Sua escolha determinará se você encontrará a saída da floresta com riqueza e sabedoria, ou se sua jornada terminará em frustração.
+            </p>
+        </div>
+    </div>
+</main>
